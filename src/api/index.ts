48f118a4a8e5ae7df478b9c30d3348ba718a1a25
aryo <muhammad.aryo@thepixelage.com>
2025-04-16 21:59:49 +0700

    refactor: rename types in api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,28 +1,30 @@
-import axios from "axios";
+import axios from 'axios';
 
 export type PopulationRecord = {
     Year: string;
     Population: number;
-}
+};
 
-export type SourceAnnotation  = {
-    source_name: string;
-    source_description: string;
-}
+export type SourceAnnotation = {
+    name: string;
+    description: string;
+};
 
 export type ApiResponse = {
     data: PopulationRecord[];
     source: {
         annotations: SourceAnnotation;
     }[];
-}
+};
 
 export const fetchPopulationData = async (): Promise<ApiResponse | null> => {
     try {
-        const { data } = await axios.get<ApiResponse>("https://datausa.io/api/data?drilldowns=Nation&measures=Population");
+        const { data } = await axios.get<ApiResponse>(
+            'https://datausa.io/api/data?drilldowns=Nation&measures=Population'
+        );
         return data;
     } catch (error) {
-        console.error("Failed to fetch data", error);
+        console.error('Failed to fetch data', error);
         return null;
     }
 };
